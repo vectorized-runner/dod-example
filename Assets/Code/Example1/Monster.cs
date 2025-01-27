11ce3c45a9f0ed1d45e6df85ed32725cceb83592
Bartu Baş <bartubas99@gmail.com>
2025-01-27 11:12:05 +0300

    opt-native-memory

--- Assets/Code/Example1/Monster.cs
+++ Assets/Code/Example1/Monster.cs
@@ -1,17 +1,18 @@
+using Unity.Collections;
 using Unity.Mathematics;
 
 namespace Code
 {
 	public struct MonsterDataArray
 	{
-		public float2[] Velocity;
-		public float[] Health;
-		public float[] HealthRegen;
-		public float[] Stamina;
-		public float[] StaminaRegen;
-		public int[] Target;
-		public float2[] Position;
-		public float[] RespawnTime;
-		public bool[] IsAlive;
+		public NativeArray<float2> Velocity;
+		public NativeArray<float> Health;
+		public NativeArray<float> HealthRegen;
+		public NativeArray<float> Stamina;
+		public NativeArray<float> StaminaRegen;
+		public NativeArray<int> Target;
+		public NativeArray<float2> Position;
+		public NativeArray<float> RespawnTime;
+		public NativeArray<bool> IsAlive;
 	}
 }
