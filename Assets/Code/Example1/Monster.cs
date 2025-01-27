e2b598b6859dd301600bc8f934c0bfa80e6281ac
Bartu Baş <bartubas99@gmail.com>
2025-01-27 10:53:54 +0300

    opt-remove-getter-setter

--- Assets/Code/Example1/Monster.cs
+++ Assets/Code/Example1/Monster.cs
@@ -4,16 +4,16 @@ namespace Code
 {
 	public struct MonsterData
 	{
-		public float2 Velocity { get; set; }
-		public float Health { get; set; }
-		public float HealthRegen { get; set; }
-		public float MaxHealth { get; set; }
-		public float Stamina { get; set; }
-		public float StaminaRegen { get; set; }
-		public float MaxStamina { get; set; }
-		public int Target { get; set; }
-		public float2 Position { get; set; }
-		public float RespawnTime { get; set; }
-		public bool IsAlive { get; set; }
+		public float2 Velocity;
+		public float Health;
+		public float HealthRegen;
+		public float MaxHealth;
+		public float Stamina;
+		public float StaminaRegen;
+		public float MaxStamina;
+		public int Target;
+		public float2 Position;
+		public float RespawnTime;
+		public bool IsAlive;
 	}
 }
